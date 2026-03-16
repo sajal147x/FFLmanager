84d78fad0072c0b409555c401eb9a2060f43b6fe
sajal gupta <sajal147x@gmail.com>
2026-03-16 17:05:55 -0500

    fully working event creation automated

--- src/main/java/com/fflmanager/events/service/CdnStorageFactory.java
+++ src/main/java/com/fflmanager/events/service/CdnStorageFactory.java
@@ -3,23 +3,30 @@ package com.fflmanager.events.service;
 import com.fflmanager.LLM.ClaudeParser;
 import com.fflmanager.LLM.FightCardParser;
 import com.fflmanager.LLM.OpenAIParser;
+import org.springframework.stereotype.Component;
 
 /**
  * Author: Sajal Gupta
  * Date: 3/16/26 3:43 PM
  */
+@Component
 public class CdnStorageFactory {
 
 
+    private final SupabaseStorage supabaseStorage;
+
+    public CdnStorageFactory(SupabaseStorage supabaseStorage) {
+        this.supabaseStorage = supabaseStorage;
+    }
     /**
      *
      * @param provider
      * @return
      */
-    public static CdnStorage getStorageService(String provider) {
+    public CdnStorage getStorageService(String provider) {
         switch (provider.toLowerCase()) {
             case "supabase":
-                return new SupabaseStorage();
+                return supabaseStorage;
             default:
                 throw new IllegalArgumentException("Unsupported provider: " + provider);
         }
--- src/main/java/com/fflmanager/events/service/EventCreationService.java
+++ src/main/java/com/fflmanager/events/service/EventCreationService.java
@@ -31,12 +31,14 @@ public class EventCreationService {
     private final FighterRepository fighterRepository;
     private final FightRepository fightRepository;
     private final FightParticipantRepository fightParticipantRepository;
+    private final CdnStorageFactory  cdnStorageFactory;
 
-    public EventCreationService(EventRepository eventRepository,  FighterRepository fighterRepository, FightRepository fightRepository,  FightParticipantRepository fightParticipantRepository) {
+    public EventCreationService(EventRepository eventRepository,  FighterRepository fighterRepository, FightRepository fightRepository,  FightParticipantRepository fightParticipantRepository,  CdnStorageFactory cdnStorageFactory) {
         this.eventRepository = eventRepository;
         this.fighterRepository = fighterRepository;
         this.fightRepository = fightRepository;
         this.fightParticipantRepository = fightParticipantRepository;
+        this.cdnStorageFactory = cdnStorageFactory;
     }
 
     /**
@@ -58,6 +60,7 @@ public class EventCreationService {
 
         //CREATE FIGHTS
         List<Fight> fights = createFights(fightCard.getFights(), event);
+
 
     }
 
@@ -142,19 +145,55 @@ public class EventCreationService {
         }
         String imageUrl = null;
         if(fighterDto.getImageUrl()!=null){
-            CdnStorage storage = CdnStorageFactory.getStorageService("supabase");
+            CdnStorage storage = cdnStorageFactory.getStorageService("supabase");
             imageUrl = storage.uploadImageToCdnStorageFromUrl(fighterDto.getImageUrl(), fighterDto.getName());
         }
         fighter = Fighter.builder()
                 .name(fighterDto.getName())
                 .weight((int) Math.round(Double.parseDouble(fighterDto.getWeight())))
                 .record(fighterDto.getRecord())
+                .height(computeHeight(fighterDto.getHeight()))
+                .reach(computeReach(fighterDto.getReach()))
+                .age(computeAge(fighterDto.getAge()))
                 .imageUrl(imageUrl)
                 .build();
         fighterRepository.save(fighter);
         return fighter;
     }
 
+    /**
+     *
+     * @param age
+     * @return
+     */
+    private int computeAge(String age) {
+        return Integer.parseInt(age.replaceAll("[^0-9]", ""));
+    }
+
+    /**
+     *
+     * @param reach
+     * @return
+     */
+    private int computeReach(String reach) {
+        Double reachDouble =   Double.parseDouble(reach.replaceAll("[^0-9.]", ""));
+        return (int) Math.round(reachDouble);
+    }
+
+    /**
+     *
+     * @param height
+     * @return
+     */
+    private Double computeHeight(String height) {
+        return Double.parseDouble(height.replaceAll("[^0-9.]", ""));
+    }
+
+    /**
+     *
+     * @param date
+     * @return
+     */
     private OffsetDateTime parseDate(String date) {
         LocalDate localDate = LocalDate.parse(date); // parses "2026-01-01"
         return localDate.atStartOfDay(ZoneOffset.UTC).toOffsetDateTime();
